0ee275ee086394b5870a19913349f31d8e54431a
pawczinx <pawczinx@gmail.com>
2016-06-27 12:33:54 +0200

    Baza Danych Logowanie

--- MTest/src/pc/controllers/LoginFormController.java
+++ MTest/src/pc/controllers/LoginFormController.java
@@ -2,14 +2,22 @@ package pc.controllers;
 
 import java.io.IOException;
 
+import pc.main.SqliteConnection;
+import java.sql.*;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class LoginFormController {
+		Connection connection;
 		private MainController mainController;
 		private String password = "zgadnij";
 		@FXML
@@ -19,15 +27,64 @@ public class LoginFormController {
 		@FXML
 		private PasswordField passwordLabel;
 		
+public LoginFormController() {
+	connection = SqliteConnection.Connector();
+	if(connection==null)System.exit(1);
+}
+
+
+public boolean isDBConnected(){
+	
+	try {
+		return !connection.isClosed();
+	} catch (Exception e) {
+		return false;
+		// TODO: handle exception
+	}
+}
+
+public boolean isLogin(String user, String pass) throws SQLException{
+	PreparedStatement preparedStatement=null;
+	ResultSet resultSet=null;
+	String query = "Select * from User where username = ? and password = ?";
+	
+	try {
+		preparedStatement = connection.prepareStatement(query);
+		preparedStatement.setString(1, user);
+		preparedStatement.setString(2, pass);
+		
+		resultSet = preparedStatement.executeQuery();
+		
+		if(resultSet.next()){
+			return true;
+		}
+		else{
+			return false;
+		}
+		
+	} catch (Exception e) {
+		return false;
+		// TODO: handle exception
+	}finally{
+		preparedStatement.close();
+		resultSet.close();
+	}
+}
+
+
 	@FXML
 	public void initialize(){
-		
+		if(isDBConnected()){
+			labelInfo.setText("Po³¹czono z Baz¹ danych");
+		}
+		else{
+			labelInfo.setText("Nie po³¹czono");
+		}
 	}
 	
 	@FXML
 	public void load(){
 		labelInfo.setText("Wciœniêto Wczytaj");
-		checkPassword();
 	}
 	
 	public void checkPassword(){
@@ -42,7 +99,36 @@ public class LoginFormController {
 		}
 		
 	}
-	
+	public void Login (ActionEvent event){
+		try {
+			if(isLogin(loginLabel.getText(), passwordLabel.getText())){
+				if(loginLabel.getText().equals("admin"))labelInfo.setText("admin");
+				else {
+					labelInfo.setText("zwykly user");
+					loadMenu();
+//					((Node)event.getSource()).getScene().getWindow().hide();
+//					Stage primaryStage = new Stage();
+//					FXMLLoader loader = new FXMLLoader();
+//					Pane root = loader.load(getClass().getResource("/application/User.fxml").openStream());
+//					UserController userController = (UserController)loader.getController();
+//					userController.getUser(txtUsername.getText());
+//					Scene scene = new Scene(root);
+//					scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//					primaryStage.setScene(scene);
+//					primaryStage.show();
+					
+					
+				}
+			}
+			else{
+				labelInfo.setText("Nie udalo sie");
+			}
+		} catch (SQLException e) {
+			labelInfo.setText("Nie udalo sie");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	public void loadMenu(){
 		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/MenuScreen.fxml"));
 		Pane pane = null;
---
+++ MTest/src/pc/main/SqliteConnection.java
@@ -0,0 +1,21 @@
+package pc.main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SqliteConnection {
+	
+	public static Connection Connector(){
+		try {
+			Class.forName("org.sqlite.JDBC");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:MTestDB.sqlite");
+			return conn;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+			// TODO: handle exception
+		}
+	}
+
+}
+
